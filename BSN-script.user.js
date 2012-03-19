69fcfdebfc740ba3a492ae03d32ae0e6e0d3e8fe
Mikkel Kjeldsen <commonquail@gmail.com>
2012-03-19 12:51:30 +0100

    Functionality: replace stunted forum search with Google site-search.

--- BSN-script.user.js
+++ BSN-script.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name          BioWare Social Network: Home
 // @namespace     quail
-// @version       1.2.1
+// @version       1.3.1
 // @updateURL     http://userscripts.org/scripts/source/127615.user.js
-// @description   Move the Group Subscriptions block in the right sidebar, when
-//                present, to the left sidebar. This is meant to be used with
-//                http://bit.ly/zDe42J, which hides the right sidebar.
+// @description   Companion script for user style http://bit.ly/zDe42J. Further
+//                details on script page.
 // @include       http://social.bioware.com/user_home.php*
+// @include       http://social.bioware.com/forum*
 // ==/UserScript==
 (function () {
-  x = document.getElementById('content_right_column').childNodes;
-  if (x[3].childNodes[1].innerHTML == 'Group Subscriptions') {
-    document.getElementById('sidebar').appendChild(x[3]);
+  if (document.URL.indexOf('user_home.php') >= 0) {
+    x = document.getElementById('content_right_column').childNodes;
+    if (x[3].childNodes[1].innerHTML == 'Group Subscriptions') {
+      document.getElementById('sidebar').appendChild(x[3]);
+    }
+  } else if (document.URL.indexOf('forum') >= 0) {
+    document.forms[1].innerHTML = '<form action="http://www.google.com/search" method="get"><div class="searchwrapper"><input type="hidden" value="social.bioware.com" name="sitesearch"><input type="text" value="" name="q" style="width:300px" class="search"><input type="submit" value="Google Search" ></div></form>';
   }
 }());
