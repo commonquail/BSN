7c83807a581ad69b8070e8ef82d2e87d2376995c
Mikkel Kjeldsen <commonquail@gmail.com>
2012-07-07 19:03:46 +0200

    Save/Edit button, $ alias, little clean-up, URL filter.

--- BSN-script.user.js
+++ BSN-script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          BioWare Social Network: Home
 // @namespace     quail
-// @version       1.4.2
+// @version       1.4.3
 // @updateURL     http://userscripts.org/scripts/source/127615.user.js
 // @description   Companion script for user style http://bit.ly/zDe42J. Further
 //                details on script page.
@@ -9,51 +9,53 @@
 // @include       http://social.bioware.com/forum*
 // @include       http://social.bioware.com/group/*/discussion/*
 // ==/UserScript==
+function $(id) { return document.getElementById(id); };
+
 (function () {
   if (document.URL.indexOf('user_home.php') >= 0) {
-    x = document.getElementById('content_right_column').childNodes;
+    var x = $('content_right_column').childNodes;
     if (x[3].childNodes[1].innerHTML == 'Group Subscriptions') {
-      document.getElementById('sidebar').appendChild(x[3]);
+      $('sidebar').appendChild(x[3]);
     }
   } else if (document.URL.indexOf('forum') >= 0) {
     document.forms[1].innerHTML = '<form action="http://www.google.com/search" method="get"><div class="searchwrapper"><input type="hidden" value="social.bioware.com" name="sitesearch"><input type="text" value="" name="q" style="width:300px" class="search"><input type="submit" value="Google Search" ></div></form>';
-    
-    
-    
-  } else {
+  } else if (document.URL.indexOf('discussion/') >= 0) {
     
     
     
     unsafeWindow.editPost = function(id) {
+      var x = $('post_' + id);
       if (unsafeWindow.isEditing) {
-        if (document.getElementById('post_edit_'+id).value === '') {
+        if ($('post_edit_' + id).value === '') {
           alert('Please enter a message to post.');
           return false;
         }
         unsafeWindow.isEditing = false;
-        document.getElementById('editPostForm').submit();
+        $('editPostForm').submit();
+        x.nextSibling.nextSibling.childNodes[1].childNodes[0].childNodes[3].childNodes[1].childNodes[1].childNodes[5].childNodes[1].innerHTML = '<img src="http://na.llnet.bioware.cdn.ea.com/u/f/eagames/bioware/social/images/icons/group_edit16.gif" class="button" style="float: left;" border="0"> Edit Post';
       } else {
         unsafeWindow.isEditing = true;
-        var postElement = document.getElementById('post_div_' + id);
+        x.nextSibling.nextSibling.childNodes[1].childNodes[0].childNodes[3].childNodes[1].childNodes[1].childNodes[5].childNodes[1].innerHTML = '<img src="http://na.llnet.bioware.cdn.ea.com/u/f/eagames/bioware/social/images/icons/group_edit16.gif" class="button" style="float: left;" border="0"> Save Post';
+        var postElement = $('post_div_' + id);
         var height = postElement.offsetHeight + 10;
-        var postText = document.getElementById('post_body_'+id).innerHTML.replace(/<br>/gi, '\r\n').replace(/>/gi, '&gt;');
+        var postText = $('post_body_' + id).innerHTML.replace(/<br>/gi, '\r\n').replace(/>/gi, '&gt;');
         var pathArray = window.location.pathname.split('/');
         var group_id = pathArray[2];
         var topic_id = pathArray[4];
         
         postElement.innerHTML = [
           "<form action='group_discussion_view.php' method='post' target='ajaxframe' name='editPostForm' id='editPostForm'>",
-          "<textarea name='grouppost_body' id='post_edit_" + id + "' style='height: " + height +"px; width: 100%;'>" + postText + "</textarea>",
+          "<textarea name='grouppost_body' id='post_edit_", id, "' style='height: ", height, "px; width:100%;'>", postText, "</textarea>",
           "<input type='hidden' name='task' value='post_edit'>",
-          "<input type='hidden' name='grouppost_id' value='" + id + "'>",
-          "<input type='hidden' name='group_id' value='" + group_id + "'>",
-          "<input type='hidden' name='grouptopic_id' value='" + topic_id + "'>",
+          "<input type='hidden' name='grouppost_id' value='", id, "'>",
+          "<input type='hidden' name='group_id' value='", group_id, "'>",
+          "<input type='hidden' name='grouptopic_id' value='", topic_id, "'>",
           "</form>"].join('');
 
         // Inject
         unsafeWindow.textarea_autogrow('post_edit_' + id);
-        document.getElementById('post_edit_' + id).focus();
+        $('post_edit_' + id).focus();
       }
-    }
+    } // editPost()
   }
 }());
